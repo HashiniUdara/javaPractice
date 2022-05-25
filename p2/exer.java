eba676d5cf6e9b874637d5541588d4e79032ba2e
HashiniUdara <hashiniudara7@gmail.com>
2022-05-25 22:27:59 +0530

    day-2

---
+++ p2/exer.java
@@ -0,0 +1,11 @@
+public class exer{
+	static String name="Bandara";
+	static int age=25;
+	public static void main(String[] args){
+	
+	
+	System.out.println("hello "+name+"you are "+age+" years old");
+	}
+
+
+}
---
+++ p2/exer2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class exer2{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter your name:");
+		Srtring name=sc.nextLine();
+		System.out.print("enter your age:");
+		int age=sc.nextlnt();
+		System.out.println("hello "+name+" you are "+age+" years old.");
+	} 
+
+}
